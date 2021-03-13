e1844d19991e2bb7f9dbda053626d098badfb363
Osman Ali Durna <o_durna@hotmail.com>
2021-03-13 19:06:40 +0200

    js data types exercises

---
+++ JsDataTypes/app.js
@@ -0,0 +1,26 @@
+var a = 3;
+console.log(a);
+var b = "3.33";
+console.log(typeof b);
+var c = 5.55;
+console.log(typeof c);
+
+var aString = "Merhaba";
+console.log(aString);
+
+var name2 = "Selam";
+var name1 = "dostum";
+var t = '" Merhaba" dedi.';
+
+console.log(name2);
+console.log(name1);
+console.log(t);
+
+console.log(`2 + 3 = ${2+3}`);
+
+// window.location = `https://${0.1+0.2}.com`;
+
+console.log(Boolean(15 > 8));
+
+var boolx = 8 < 4;
+console.log(boolx);
