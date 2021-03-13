f524cd091dc0baf6716adf4ce6e10f7277964116
Osman Ali Durna <o_durna@hotmail.com>
2021-03-13 17:21:50 +0200

    updated

---
+++ JsStart/app.js
@@ -0,0 +1,11 @@
+/*document.write("Hello");
+document.write("<br>"); // document.write("\n") işe yaramaz.
+document.write("Clarusway");
+// console.log("Hello \nClarusway");
+alert("Hello \nClarusway")
+confirm("Hello \nClarusway")
+prompt("Yasiniz " + 17)
+*/
+
+document.body.innerHTML="<h1>Hello <br> Clarusway</h1>"
+console.log(typeof(42));
---
+++ Jsintro/app.js
@@ -0,0 +1,5 @@
+var x = 5;
+var y = 7;
+y = x;
+
+console.log(y)
